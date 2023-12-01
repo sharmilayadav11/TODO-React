31effc783648702be93d1fa0b7ed24c63f681022
Sharmila Yadav <122268263+sharmilayadav11@users.noreply.github.com>
2023-12-01 13:33:51 +0200

    Made more components: Todo, Todos, Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './MyComponent/Header';
+import { TodoItem } from './MyComponent/TodoItem';
+import { Todos } from './MyComponent/Todos';
+import { Footer } from './MyComponent/Footer';
 
 function App() {
   return (
     <>
       <Header />
+      <Todos />
+      <TodoItem />
+      <Footer />
     </>
   );
 }

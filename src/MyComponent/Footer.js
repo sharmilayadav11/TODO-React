4e8bedd046c1c9b11c7b91ebb3d48841555c91ab
Sharmila Yadav <122268263+sharmilayadav11@users.noreply.github.com>
2023-12-14 22:57:21 +0200

    Footer and writing css in react

--- src/MyComponent/Footer.js
+++ src/MyComponent/Footer.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
 export const Footer = () => {
+    let footerStyle = {
+        position: "relative",
+        top: "40vh",
+        width: "100%",
+
+    }
     return (
-        <div>Footer Works!</div>
+        <footer className='bg-dark text-light py-3' style={footerStyle}>
+            <p className="text center">
+                Copyright &copy; MyTodosList.com
+            </p>
+
+
+        </footer>
     )
 }
